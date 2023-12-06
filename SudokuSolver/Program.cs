0f40ffb238d4e241f604ac5a674446ddf4b05caf
QuinReef <quinreefyt@gmail.com>
2023-12-06 17:43:59 +0100

    Added automatic random fill, changed the OOP to Array Tuple representation and started on the evaluation functiom

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -1,64 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using static Sudoku.SudokuSolver;
+
 namespace Sudoku
 {
-    class Square
-    {
-        //Number is always between 0 - 9; 
-        public ushort value;
-        public bool isStatic;
-    }
-    class Cluster
-    {
-        //Is 2d array faster than a 1d array;
-        public Square[,] cluster = new Square[3,3];
-        //public Square[] cluster = new Square[9];
-
-    }
     class SudokuSolver
     {
-        public List<int[,]> sudokuGrids = new();
+        public List<SudokuGrid> sudokuGrids = new List<SudokuGrid>();
 
-        static Random random = new();
+        static Random random = new Random();
 
-        int[,] currentGrid = new int[9,9];
+        public SudokuGrid currentGrid;
+
 
         public SudokuSolver(string path)
         {
             LoadSudokuFromFile(path);
-            currentGrid = sudokuGrids[0];
-            Console.WriteLine("Score:" + EvaluateRows());
+            FillMissingValues();
+            Console.WriteLine("Score: " + EvaluateGrid(currentGrid));
         }
 
-        //Read Sudoku Puzzle from a File
+        //Tuple representing a cell in the Sudoku
+        public record SudokuCell(ushort Value, bool IsStatic);
+
+        // Named 2D Array representing the Sudoku grid
+        public record SudokuGrid(SudokuCell[,] Cells);
+
+        // Read Sudoku Puzzle from input file
         void LoadSudokuFromFile(string filePath)
         {
             try
             {
-                //Get entire input
                 string[] lines = File.ReadAllLines(filePath);
 
-                //Only process the lines with data
+                // Only process the lines with data
                 for (int k = 1; k < lines.Length; k += 2)
                 {
-                    //As the first input is " ", shift the input 1 to the left 
+                    // As the first input is " ", shift the input 1 to the left 
                     string[] values = lines[k].Split(' ').Skip(1).ToArray();
 
-                    int[,] sudokuFields = new int[9, 9];
+                    SudokuCell[,] sudokuCells = new SudokuCell[9, 9];
 
-                    //Counter for item in the values list.
+                    // Counter for item in the values list.
                     int valCounter = 0;
                     for (int i = 0; i < 9; i++)
                     {
                         for (int j = 0; j < 9; j++)
                         {
-                            sudokuFields[i, j] = int.Parse(values[valCounter]);
+                            ushort itemVal = ushort.Parse(values[valCounter]);
+                            bool isStatic = itemVal != 0;
+                            sudokuCells[i, j] = isStatic ? new SudokuCell(itemVal, true) : new SudokuCell(0, false);
+
                             valCounter++;
                         }
                     }
-                    //Add to class variable
-                    sudokuGrids.Add(sudokuFields);
+
+                    // Add to class variable
+                    sudokuGrids.Add(new SudokuGrid(sudokuCells));
                 }
+
+                // Initialize currentGrid with the first Sudoku grid
+                currentGrid = sudokuGrids.First();
             }
             catch (Exception ex)
             {
@@ -66,28 +70,71 @@ namespace Sudoku
             }
         }
 
-        //Represent Sudoku Puzzle
-        public static void PrintGrid(int[,] grid)
+        // Fill missing values in each 3x3 grid with random numbers
+        private void FillMissingValues()
         {
-            //Top Line
+            for (int i = 0; i < 9; i += 3)
+            {
+                for (int j = 0; j < 9; j += 3)
+                {
+                    //TODO: Get this data while reading the input file
+                    // Create a list of available numbers (1-9)
+                    List<ushort> availableNumbers = Enumerable.Range(1, 9).Select(x => (ushort)x).ToList();
+
+                    // Remove numbers already present in the current 3x3 grid
+                    for (int row = i; row < i + 3; row++)
+                    {
+                        for (int col = j; col < j + 3; col++)
+                        {
+                            if (currentGrid.Cells[row, col].IsStatic)
+                            {
+                                availableNumbers.Remove(currentGrid.Cells[row, col].Value);
+                            }
+                        }
+                    }
+
+                    // Fill each cell in the 3x3 grid
+                    for (int row = i; row < i + 3; row++)
+                    {
+                        for (int col = j; col < j + 3; col++)
+                        {
+                            if (!currentGrid.Cells[row, col].IsStatic)
+                            {
+                                // Choose a random number from the available numbers
+                                int randomIndex = random.Next(availableNumbers.Count);
+                                currentGrid.Cells[row, col] = new SudokuCell(availableNumbers[randomIndex], false);
+
+                                // Remove the chosen number from the available numbers
+                                availableNumbers.RemoveAt(randomIndex);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Represent Sudoku Puzzle
+        public static void PrintGrid(SudokuGrid grid)
+        {
+            // Top Line
             Console.Write("\n------------------\n");
 
-            //For each Row
+            // For each Row
             for (int i = 0; i < 9; i++)
             {
                 Console.Write("|");
 
-                //For each Collumn
+                // For each Column
                 for (int j = 0; j < 9; j++)
                 {
-                    //Every third item print a vertical line
+                    // Every third item print a vertical line
                     if (j % 3 == 2)
-                        Console.Write(grid[i, j] + "|");
+                        Console.Write(grid.Cells[i, j].Value + "|");
                     else
-                        Console.Write(grid[i, j] + " ");
+                        Console.Write(grid.Cells[i, j].Value + " ");
                 }
 
-                //Every third row print an divider 
+                // Every third row print a divider 
                 if (i % 3 == 2)
                     Console.Write("\n------------------\n");
                 else
@@ -95,26 +142,53 @@ namespace Sudoku
             }
         }
 
-        private int EvaluateRows()
+
+        private int EvaluateGrid(SudokuGrid grid)
         {
             int score = 0;
+
+            // Evaluate rows
             for (int i = 0; i < 9; i++)
             {
+                HashSet<ushort> rowNumbers = new HashSet<ushort>();
                 for (int j = 0; j < 9; j++)
                 {
-                    if(currentGrid[i, j] == 0)
-                        score++;
+                    if (grid.Cells[i, j].Value != 0)
+                    {
+                        rowNumbers.Add(grid.Cells[i, j].Value);
+                    }
                 }
+                score += 9 - rowNumbers.Count; // Number of missing values in the row
             }
+
+            // Evaluate columns
+            for (int j = 0; j < 9; j++)
+            {
+                HashSet<ushort> colNumbers = new HashSet<ushort>();
+                for (int i = 0; i < 9; i++)
+                {
+                    if (grid.Cells[i, j].Value != 0)
+                    {
+                        colNumbers.Add(grid.Cells[i, j].Value);
+                    }
+                }
+                score += 9 - colNumbers.Count; // Number of missing values in the column
+            }
+
             return score;
         }
+
     }
+
     class Program
     {
         static void Main(string[] args)
         {
-            SudokuSolver sv = new("../../../sudoku_input.txt");
-            SudokuSolver.PrintGrid(sv.sudokuGrids[0]);
+            SudokuSolver sv = new SudokuSolver("../../../sudoku_input.txt");
+            SudokuSolver.PrintGrid(sv.currentGrid);
+            SudokuSolver.PrintGrid(sv.currentGrid);
+
         }
     }
+
 }
