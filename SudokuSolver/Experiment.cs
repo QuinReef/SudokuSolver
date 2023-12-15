cf9c8420473b90eeb6da2c4016daf072fcb6e21a
Mathieu Chappin <Chaka811@live.nl>
2023-12-15 16:27:49 +0100

    Added dynamic sample sizes. This should be kept to 4 or 5 for optimal performance.

--- SudokuSolver/Experiment.cs
+++ SudokuSolver/Experiment.cs
@@ -4,7 +4,7 @@ public class Experiment
 {
     private List<ushort> _randomWalkExperimentSizes =
         new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 25, 30, 40,50,100,200 };
-    private const int _sampleSize = 10;
+    private List<ushort> _sampleSizes = new() {5};
     public Experiment()
     {
  
@@ -14,17 +14,20 @@ public class Experiment
     {
         using (StreamWriter writer = new StreamWriter($"../../../testSizeResults.txt"))
         {
-            foreach (var testSize in _randomWalkExperimentSizes)
-            {
-                writer.Write($"{testSize}:");
-                for (int i = 0; i < _sampleSize; i++)
-                {
-                    SudokuSolver solver = new((Sudoku)subject.Clone(), testSize, false);
-                    long timeMili = solver.HillClimbing();
-                    writer.Write($"{timeMili},");
-                }
-                writer.WriteLine();
+            foreach (var sampleSize in _sampleSizes) {
+                    Console.WriteLine(sampleSize);
+                    writer.WriteLine(sampleSize);
+                foreach (var testSize in _randomWalkExperimentSizes) {
+                    writer.Write($"{testSize}:");
+                    for (int i = 0; i < sampleSize; i++) {
+                        SudokuSolver solver = new((Sudoku)subject.Clone(), testSize, false);
+                        long timeMili = solver.HillClimbing();
+                        writer.Write($"{timeMili},");
+                    }
 
+                    writer.WriteLine();
+
+                }
             }
         }
 
