fc1538351ec48dfba525368c3f3a04b59b292944
QuinReef <quinreefyt@gmail.com>
2024-01-23 20:17:35 +0100

    FCMCV

--- SudokuSolver/SudokuSolvers/SudokuSolverFCMCV.cs
+++ SudokuSolver/SudokuSolvers/SudokuSolverFCMCV.cs
@@ -1,83 +1,78 @@
 ﻿namespace SudokuSolver.SudokuSolvers;
 
-public class SudokuSolverFCMCV : SudokuSolverFC {
+public class SudokuSolverFCMCV : SudokuSolverFC
+{
     public SudokuSolverFCMCV(Sudoku sudoku, bool showSteps) : base(sudoku, showSteps) { }
 
-    public override void Solve() {
+    public override void Solve()
+    {
         /* Firstly, make the initial Sudoku node consistent by updating domains based on fixed values,
            then start solving using Forward Checking. */
         MakeNodeConsistent();
         SolveSudoku();
     }
 
-    protected override Tuple<Cell, (ushort, ushort)>? FindEmptyCeell() {
-        int minDomainSize = int.MaxValue;
-        (ushort cl, ushort r) = (0, 0);
-        bool b = false;
-        Cell c = new Cell(0, false);
+    protected Tuple<Cell, (ushort, ushort)>? FindSmallestEmptyCellByDomainSize()
+    {
+        Tuple<Cell, (ushort, ushort)> smallestEmptyCell = null;
 
-        for (ushort row = 0; row < 9; row++) {
-            for (ushort column = 0; column < 9; column++) {
-                c = ActiveSudoku.GetSudokuGrid()[row / 3 * 3 + column / 3].RetrieveCells()[column % 3, row % 3];
-                if (c.Value == 0) { // empty
-                    int domainSize = c.Domain?.Count ?? 0;
+        for (ushort row = 0; row < 9; row++)
+        {
+            for (ushort column = 0; column < 9; column++)
+            {
+                Cell cell = ActiveSudoku.GetSudokuGrid()[row / 3 * 3 + column / 3].RetrieveCells()[column % 3, row % 3];
 
-                    if (domainSize < minDomainSize) {
-                        minDomainSize = domainSize;
-                        cl = column; r = row;
-                        b = true;
+                if (cell.Value == 0)
+                {
+                    int domainSize = cell.Domain?.Count ?? 0;
+
+                    if (smallestEmptyCell == null || domainSize < smallestEmptyCell.Item1.Domain?.Count)
+                    {
+                        smallestEmptyCell = new Tuple<Cell, (ushort, ushort)>(cell, (row, column));
                     }
                 }
             }
         }
 
-        if (!b) {
-            Console.WriteLine($"| {c} |");
-        }
-
-        Console.WriteLine($"{cl} - {r}");
-        return !b ? null : new Tuple<Cell, (ushort, ushort)>(c, (cl, r));
+        return smallestEmptyCell;
     }
 
     private bool SolveSudoku()
     {
+        Tuple<Cell, (ushort, ushort)> emptyCell = FindSmallestEmptyCellByDomainSize();
+
         // If no empty cells remain, the puzzle is solved.
-        if (CheckIfDone(out Tuple<Cell, (ushort, ushort)>? emptyCell)) {
+        if (emptyCell == null)
+        {
+            ActiveSudoku.Show();
             return true;
         }
 
         // Print relevant information to the console.
         Print();
 
-        // Obtain the coordinates of the first empty cell in the sudoku.
-        (ushort row, ushort column) = FindEmptyCeell().Item2;
 
-        // Try assigning values from the domain to the empty cell.
-        foreach (ushort value in new HashSet<ushort>(emptyCell.Item1.Domain!))
+        (Cell emptyCellObject, (ushort row, ushort column)) = emptyCell;
+
+        foreach (ushort value in new HashSet<ushort>(emptyCellObject.Domain!))
         {
             if (IsValidMove(ActiveSudoku, row, column, value))
             {
                 SudokuCluster cluster = ActiveSudoku.GetSudokuGrid()[row / 3 * 3 + column / 3];
 
-                // Assign the value to the empty cell.
-                PerformMove((column, row), cluster, new Cell(value, emptyCell.Item1.IsFixed, emptyCell.Item1.Domain!));
+                PerformMove((column, row), cluster, new Cell(value, emptyCellObject.IsFixed, new HashSet<ushort>()));
 
-                // Update the domains of the affected cells.
                 UpdateDomains((column, row), cluster, value, false);
-
-                // Recursively try to solve the rest of the puzzle.
                 if (SolveSudoku())
                 {
                     return true;
                 }
 
-                // If the previous move had no availible digits, undo the move and restore the domains.
-                PerformMove((column, row), cluster, emptyCell.Item1);
+                PerformMove((column, row), cluster, emptyCellObject);
                 UpdateDomains((column, row), cluster, value, true);
             }
         }
 
-        // No valid value was found for the current empty cell.
         return false;
     }
 }
